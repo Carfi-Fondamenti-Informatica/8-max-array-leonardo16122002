ca3d559ce3e3303fde2f37f5636e82cc20940182
leonardo16122002 <78797793+leonardo16122002@users.noreply.github.com>
2021-12-07 11:35:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+//
+// Created by leona on 07/12/2021.
+//
 
+#include "lib.h"
+
+float max(float n[],int d){
+    float m=n[0];
+    int i;
+
+    for(i=1;i<d;i++){
+        if(n[i]>m){
+            m=n[i];
+        }
+    }
+    return m;
+}
